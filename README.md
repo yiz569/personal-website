49941d86b3491928905cf6e0591c15cff43a150f
Yifeng <yiz569@ucsd.edu>
2019-04-16 16:52:19 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+# Yifeng Zhang Website
 This repository contains the source code of my personal website
 which is built with React.
--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     });
   }
 
-  componentDidMount(){
+  componentWillMount(){
     this.getResumeData();
   }
 
